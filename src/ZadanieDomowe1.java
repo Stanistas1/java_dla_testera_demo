1c730bf28215c02327259793072563d6e1b40765
Stanistas1 <stachu.skamrot@gmail.com>
2024-04-28 14:19:29 +0200

    Added zadanie domowe 1

---
+++ src/ZadanieDomowe1.java
@@ -0,0 +1,30 @@
+public class ZadanieDomowe1 {
+    public static void main(String[] args) {
+        int jeden = 1212;
+        int dwa = 32;
+        int trzy = 123;
+        int cztery = 3;
+
+        int sumaJedenDwa = jeden + dwa;
+
+        if (sumaJedenDwa % 2 == 0) {
+  /*          System.out.println("Suma jest parzysta");
+        }else{
+            System.out.println("Suma jest nieparzysta");
+    */
+        }
+
+        int sumaTrzyCztery = trzy + cztery;
+  /*      if (sumaTrzyCztery % 2 == 0) {
+            System.out.println("Suma jest parzysta");
+        }else{
+            System.out.println("Suma jest nieparzysta");
+        }
+*/
+        if (sumaJedenDwa % 2 == 0 && sumaTrzyCztery % 2 == 0) {
+            System.out.println("Suma jest parzysta");
+        } else {
+            System.out.println("Suma jest nieparzysta");
+        }
+    }
+}
